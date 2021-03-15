b2b7df81c7a234915626fc47230d125ac3a621bd
Mujtaba-cpu <80210365+Mujtaba-cpu@users.noreply.github.com>
2021-03-15 10:18:47 +0500

    Update HotdogRunner.java

--- Lab-4/Assingment/HotdogRunner.java
+++ Lab-4/Assingment/HotdogRunner.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Assingment;
 
-/**
- *
- * @author BOSS
- */
+
 public class HotdogRunner {
     public static void main(String[] args){
         Hotdog h1 = new Hotdog(101,0);
